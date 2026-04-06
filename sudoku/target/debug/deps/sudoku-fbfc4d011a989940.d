8a0f690db5a61d60ea86584bb7701e4b527c0ec3
marysia <mszarata@student.agh.edu.pl>
2026-04-06 14:34:07 +0200

    next fix

--- sudoku/target/debug/deps/sudoku-fbfc4d011a989940.d
+++
@@ -1,5 +0,0 @@
-C:\Users\basia\Desktop\studia\sem4\rust_first_steps\sudoku\target\debug\deps\sudoku-fbfc4d011a989940.d: src\main.rs
-
-C:\Users\basia\Desktop\studia\sem4\rust_first_steps\sudoku\target\debug\deps\sudoku-fbfc4d011a989940.exe: src\main.rs
-
-src\main.rs:
--- sudoku/target/debug/sudoku.d
+++
@@ -1,1 +0,0 @@
-C:\Users\basia\Desktop\studia\sem4\rust_first_steps\sudoku\target\debug\sudoku.exe: C:\Users\basia\Desktop\studia\sem4\rust_first_steps\sudoku\src\main.rs
